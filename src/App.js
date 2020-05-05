b890d26b7544fd345ada86fef2d2ebaad33e1854
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-05-05 17:53:39 +0200

    change function to class + create table + mount tab into state

--- src/App.js
+++ src/App.js
@@ -2,17 +2,64 @@ import React from 'react';
 import './App.css';
 
 const initialProductList = [
-  { id: 1, name: 'produit 1', price: 50, quantity: 1 },
-  { id: 2, name: 'produit 2', price: 75, quantity: 2 },
-  { id: 3, name: 'produit 3', price: 20, quantity: 5 }
+  { id: 1, name: 'produit 1', price: 50, quantity: 1, },
+  { id: 2, name: 'produit 2', price: 75, quantity: 2, },
+  { id: 3, name: 'produit 3', price: 20, quantity: 5, }
 ];
 
-function App () {
-  return (
-    <div className='App'>
-      <h1>Ma commande</h1>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    products : []
+  }
+
+  getQuantity = (e) => {
+    const products = this.state.products
+    const productIndex = products.findIndex(item => item.id == e.target.name)
+    products[productIndex].quantity = e.target.value
+    this.setState({ products : products })
+  }
+
+  add = () => {
+    let result = 0
+    for (let i = 0; i < this.state.products.lenght ; i++){
+      console.log(result);
+      result = result + (this.state.products.quantity * this.state.products.price)
+    }
+    return result
+  }
+
+  componentDidMount() {
+    this.setState({ products : initialProductList })
+  }
+  
+  render() {
+    return (
+      <div className='App'>
+        <h1>Ma commande</h1>
+        <table>
+          <tbody>
+            <tr>
+              <th>Produit</th>
+              <th>Prix unitaire</th>
+              <th>Quantité</th>
+              <th>Prix Total</th>
+            </tr>
+              {this.state.products.map((product, i) =>
+              <tr key={i}>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td><input name={product.id} value={product.quantity} onChange={this.getQuantity} type="number"/></td>
+                <td>{product.quantity * product.price}</td>
+                {/* {initialProductList[i].push((product.quantity * product.price))} */}
+              </tr>
+                )}
+          </tbody>
+        </table>
+        <p>Total du panier : {this.add}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
