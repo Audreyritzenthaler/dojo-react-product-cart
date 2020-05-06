120ade81cc07adb411109de25eef428334e9880f
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-05-06 14:29:26 +0200

    tests steps + bonus

--- src/App.js
+++ src/App.js
@@ -4,34 +4,36 @@ import './App.css';
 const initialProductList = [
   { id: 1, name: 'produit 1', price: 50, quantity: 1, },
   { id: 2, name: 'produit 2', price: 75, quantity: 2, },
-  { id: 3, name: 'produit 3', price: 20, quantity: 5, }
+  { id: 3, name: 'produit 3', price: 20, quantity: 5, },
 ];
-
 
 class App extends React.Component {
   state = {
-    products : []
+    products : [...initialProductList]
   }
 
   getQuantity = (e) => {
     const products = this.state.products
     const productIndex = products.findIndex(item => item.id == e.target.name)
-    products[productIndex].quantity = e.target.value
+    if (e.target.value == 0){
+      const dial = window.confirm( "Do you want delete this item ?" )
+      if (dial == true) {
+        products.splice(productIndex, 1)
+      }
+    } else {
+        products[productIndex].quantity = Number(e.target.value)
+    }
     this.setState({ products : products })
   }
 
-  add = () => {
-    let result = 0
-    for (let i = 0; i < this.state.products.lenght ; i++){
-      console.log(result);
-      result = result + (this.state.products.quantity * this.state.products.price)
-    }
-    return result
+  total = (reste, courant) => {
+    const  sum = (reste.price * reste.quantity) + (courant.price * courant.quantity)
+    return {price: sum, quantity: 1}
   }
 
-  componentDidMount() {
-    this.setState({ products : initialProductList })
-  }
+  // componentDidMount() {
+  //   this.setState({ products : initialProductList })
+  // }
   
   render() {
     return (
@@ -51,13 +53,26 @@ class App extends React.Component {
                 <td>{product.price}</td>
                 <td><input name={product.id} value={product.quantity} onChange={this.getQuantity} type="number"/></td>
                 <td>{product.quantity * product.price}</td>
-                {/* {initialProductList[i].push((product.quantity * product.price))} */}
               </tr>
                 )}
           </tbody>
         </table>
-        <p>Total du panier : {this.add}</p>
-      </div>
+        <p>Total du panier : {this.state.products.reduce(this.total).price} </p>
+          
+          <form>
+          <h2>Ajouter un produit</h2>
+            <div className="field">
+              <label htmlFor="name">Nom</label>
+              <input name="name" type="text" required/> 
+            </div>
+
+            <div className="field">
+              <label htmlFor="price">Prix</label>
+              <input name="price" type="number" required/>
+            </div>
+            <button>Ajouter</button>
+          </form>
+        </div>
     );
   }
 }
